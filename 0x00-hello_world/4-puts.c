c45b2c88faae9d0c6c6bcad29e8e23ec670eecf2
willzekuri <willzekuri@gmail.com>
2023-03-16 15:55:31 +0000

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzl:e");
 	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - entry point
+ *
+ * demonstrates the usage of printf
+ * returns 0 which means success
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
