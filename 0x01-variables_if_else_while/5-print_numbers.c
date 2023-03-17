9242d5c2dbfc1b506ee21ae786cb052b042f445b
willzekuri <willzekuri@gmail.com>
2023-03-17 22:56:37 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ **main - Entry point
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
