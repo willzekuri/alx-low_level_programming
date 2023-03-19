2f957a302a17f969fc3b27c7ce12e6aec0a86460
willzekuri <willzekuri@gmail.com>
2023-03-19 22:41:24 +0000

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 99; x++)
+	{
+		for (y = x + 1; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 98 && y == 99)
+				continue;
+			putchar(',');
+		}
+	}
+
+
+	putchar('\n');
+
+	return (0);
+}
