f7c4baba2dc511986f13eb2f24b14c3d5aa73911
willzekuri <willzekuri@gmail.com>
2023-03-19 22:13:36 +0000

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb_4.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+
+				if ((a != b) && (b != c))
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+
+					if ((a == 7) && (b == 8) && (c == 9))
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
