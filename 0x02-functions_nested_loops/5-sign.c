342457931843b2c49af530f03c9ab617190875b0
willzekuri <willzekuri@gmail.com>
2023-03-21 23:49:49 +0000

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number
+ * @n: the parameter and argument
+ * Return: 1(if n is greater than 0), 0(if n is 0) and -1(if n is less than 0);
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+
+	return (n);
+}
