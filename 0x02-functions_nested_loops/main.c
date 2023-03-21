416e2e9e5dce868fbbbf2dcf253464a2c8c97ec0
willzekuri <willzekuri@gmail.com>
2023-03-21 19:11:50 +0000

    task 1

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "mains.h"
 /**
 *main - Entry point
 *
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
 int _putchar(char );
+void print_alphabet(void)
+{
+	int a = 'a';
+	while(a <= 'z')
+	{
+		_putchar('a');
+		a++;
+	}
+	return (a);
+}
