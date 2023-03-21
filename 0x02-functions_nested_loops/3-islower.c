7ed1d4b2c138c3e7bef06436632bc5f5d0511b2d
willzekuri <willzekuri@gmail.com>
2023-03-21 23:07:03 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if argument is lowercase and 0 otherwise
+ *
+ *c: takes in argument
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+
+	return (c);
+}
