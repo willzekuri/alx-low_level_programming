7d3c73563909ad61cdf87d2947622cffc0fe50ac
willzekuri <willzekuri@gmail.com>
2023-03-21 23:55:29 +0000

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,24 +10,20 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-
 		return (1);
+		_putchar(' ');
 	}
 	if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
+		_putchar(' ');
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
+		_putchar(' ');
 	}
 
 	return (n);
