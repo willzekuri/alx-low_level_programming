a20ae14274cd30c26da81084c15c0dc4f8ef14e7
willzekuri <willzekuri@gmail.com>
2023-03-22 12:32:08 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,19 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		while (n > 98)
+		{
 		printf("%i, ", n);
 		n--;
+		}
 	}
+	_putchar('\n');
 	while (n <= 98)
 	{
 		if (n != 98)
-		{
 			printf("%i, ", n);
-		}
 		else
 			printf("%i", n);
+
 		n++;
 	}
 }
