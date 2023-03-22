32851bfaf7da1a70e486e0ea6e32e1cb322286a6
willzekuri <willzekuri@gmail.com>
2023-03-22 13:04:04 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,24 +15,24 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (c > 9)
+			if (b == 0)
 			{
-				_putchar(c / 10 + '0');
-				_putchar(c % 10 + '0');
-				if (b == 9)
-				{
-					continue;
-				}
+				_putchar(c + '0');
+			}
+
+			if (c < 10 && b != 0)
+			{
 				_putchar(',');
 				_putchar(' ');
-			}
-			_putchar(c + '0');
-			if (b == 9)
+				_putchar(' ');
+				_putchar(c + '0');
+			} else if (c >= 10)
 			{
-				continue;
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
 	_putchar('\n');
 	}
