36d19f9fae9088ca45123909c35fe753ec7bc75d
willzekuri <willzekuri@gmail.com>
2023-03-22 11:11:15 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *add - sums 2 numbers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: result (the sum of 2 numbers)
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+
+}
