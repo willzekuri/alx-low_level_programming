d3d1f04bd73aa5238966647436179a70b7b9be32
willzekuri <willzekuri@gmail.com>
2023-03-22 22:13:42 +0000

    task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,24 @@
-#include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int a;
+	int sum;
+
+	sum = 0;
+	for (a = 0; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			sum += a;
+	}
+	printf("%i\n", sum);
+
+	return (0);
+}
 
---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,28 @@
+#include "mains.h"
+#include "1-alphabet.c"
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case 10 times
+ *
+ * Return: Always 0.
+ */
+
+int print_alphabet_x10(void)
+{
+	int a = 'a';
+	int j;
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+
+	while (j < 9)
+	{
+		print_alphabet();
+		j++;
+	}
+
+	return (j);
+}
