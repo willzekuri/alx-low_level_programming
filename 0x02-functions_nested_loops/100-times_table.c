482154ab0642fce15c06d5c16631df805576e9a0
willzekuri <willzekuri@gmail.com>
2023-03-22 17:53:17 +0000

    task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,62 @@
+#include "main.h"
+/**
+ *print_times_table - prints any times table passed as argument
+ * @n: parameters
+ *
+ */
+void print_times_table(int n)
+{
+	int a;
+	int b;
+	int c;
+
+	if (n > 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+
+				if (c < 10)
+				{
+					_putchar(c + '0');
+					if (b == n)
+					{
+						break;
+					}
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				if (c >= 10 && c <= 99)
+				{
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+					if (b == n)
+					{
+						break;
+					}
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				if (c > 99 && c < 999)
+				{
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+					if (b == n)
+					{
+						break;
+					}
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+
+			}
+			_putchar('\n');
+		}
+	}
+}
