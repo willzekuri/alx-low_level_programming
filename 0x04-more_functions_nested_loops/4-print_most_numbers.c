2a513029e6651311815eee7bc5fe2b80236473cd
willzekuri <willzekuri@gmail.com>
2023-03-23 22:41:03 +0000

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_most_numbers - used to print numbers from 0 - 9 except 2 and 4
+*
+*Return: void
+*/
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
