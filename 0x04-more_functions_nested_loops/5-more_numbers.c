be0135bf024d9543eee4e2fc90b0b7ff805ee372
willzekuri <willzekuri@gmail.com>
2023-03-23 22:59:02 +0000

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*more_numbers - used to print numbers from 0 - 14 on new line 10 times
+*
+*Return: void
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			} else
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
