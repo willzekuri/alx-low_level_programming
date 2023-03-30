6066c5938bfdc1de76041f0de28ac21871e39cdb
willzekuri <willzekuri@gmail.com>
2023-03-30 00:10:46 +0000

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_puts - A function to print string, followed by new line.
+*@str: pointer parameter
+*
+*/
+
+void _puts(char *str)
+{
+
+	for (; *str != '\0'; str++)
+	{
+		if (*str != '\0')
+		{
+			_putchar(*str);
+		}
+		
+	}
+	_putchar('\n');
+} 
