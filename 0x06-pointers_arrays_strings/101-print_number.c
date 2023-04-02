82326144e377dabc882e313027e1e97d912056ae
willzekuri <willzekuri@gmail.com>
2023-04-02 19:40:11 +0000

    task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,41 +7,14 @@
 
 void print_number(int n)
 {
-
-	int m = n;
-
 	if (n < 0)
 	{
-		m = -n;
 		_putchar('-');
-		_putchar(m / 10 + '0');
-		_putchar(m % 10 + '0');
+		n = -n;
 	}
-	else if (n == 0)
+	if (n > 9)
 	{
-		_putchar(n + '0');
+		print_number(n / 10);
 	}
-	else if (n > 0 && n < 10)
-	{
-		_putchar(n + '0');
-	}
-	else if (n > 10 && n < 100)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 100 && n < 1000)
-	{
-		_putchar(n / 100 + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 1000 && n < 10000)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar(((n % 1000) / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar(n % 10 + '0');
-	}
-
+	_putchar(n % 10 + '0');
 }
