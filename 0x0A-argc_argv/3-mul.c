cac24b94a676a2b7631f9c8024f021d491bcdeb9
willzekuri <willzekuri@gmail.com>
2023-04-10 16:27:38 +0000

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*multiply - Function to multiply two numbers
+*@a: parameter 1
+*@b: second parameter
+*Return: result of both parameters multiplied
+*/
+int multiply(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
+/**
+*main - multiplies 2 numbers
+*@argc: Argument counter
+*@argv: Argument vector
+*Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	printf("%d\n", multiply(x, y));
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,3 +2,5 @@ int _putchar(char c);
 int main(int argc, char* argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char* argv[]);
+int multiply(int a, int b);
+int main(int argc, char* argv[]);
