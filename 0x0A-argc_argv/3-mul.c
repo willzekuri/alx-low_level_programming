99ce18af54a13525b53d3960c929dd0cb251be87
willzekuri <willzekuri@gmail.com>
2023-04-10 21:16:26 +0000

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -25,14 +25,16 @@ int main(int argc, char *argv[])
 {
 	int x, y;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 	}
-
+	else
+	{
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
 
 	printf("%d\n", multiply(x, y));
+	}
 	return (0);
 }
