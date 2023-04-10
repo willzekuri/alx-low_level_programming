f7e85fb0164d08d6b1cc153c119e9833dd80e174
willzekuri <willzekuri@gmail.com>
2023-04-10 14:00:09 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Function to print the name of program
+*@argc: stores the number of argument used to call the program
+*@argv: array of characters that stores the argument used to call the program
+*Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
