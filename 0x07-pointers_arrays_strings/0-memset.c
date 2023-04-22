ed8b4aa29932e30396a08b6391b03f45d330811e
willzekuri <willzekuri@gmail.com>
2023-04-22 11:46:56 +0000

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_memset - Function thst fills memory with constant byte
+*@s: parameter for memory area
+*@b: parameter 2 which is the constant byte
+*@n: unsigned integer parameter which is bytes of the memory area
+*
+*Return: pointer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
+}
